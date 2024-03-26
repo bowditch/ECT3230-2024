92805ea992ceda68cb9565ded5afe5440f888e82
Bowditch <bowditch@ohio.edu>
2024-03-26 13:48:53 -0400

    Drawer Feature

--- XRI_Project/Assets/CabinEscape/Scripts/DrawerFeatures.cs
+++ XRI_Project/Assets/CabinEscape/Scripts/DrawerFeatures.cs
@@ -29,6 +29,47 @@ public class DrawerFeatures : CoreFeatures
   
     void Start()
     {
-        
+
+
+        //drawers with simple interactable
+         simpleInteractable?.selectEntered.AddListener((s) =>
+         {
+
+             //if drawer is not open
+             if (!open)
+             {
+                 OpenDrawer();
+             }
+         });
+    }
+    private void OpenDrawer()
+    {
+        open = true;
+        PlayOnStart();
+        StartCoroutine(ProcessMotion());
+    }
+
+    private IEnumerator ProcessMotion()
+    {
+        while (open)
+        {
+            if (featureDirection == FeatureDirection.Forward && drawerSlide.localPosition.z >= maxDistance)
+            {
+                drawerSlide.Translate(Vector3.forward * Time.deltaTime * speed);
+            }
+
+            else if (featureDirection == FeatureDirection.Backward && drawerSlide.localPosition.z <= maxDistance)
+            {
+
+                drawerSlide.Translate(-Vector3.forward * Time.deltaTime * speed);
+            }
+
+            else
+            {
+                open = false; //Ends the looping
+            }
+
+            yield return null;
+        }
     }
 }
